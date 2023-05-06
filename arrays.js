b20e2ceaca90740332c860ea76f16af90ea2c0b8
Angiefiona <angellamumoh@gmail.com>
2023-05-06 20:45:06 +0300

    changes

--- arrays.js
+++ arrays.js
@@ -4,8 +4,6 @@ function reverseString(str){
     str = str.map(str => str.split('').reverse().join);
     str.reverse();
     }
-   
-   
    let string = ["Terry", "Chozi", "Gracey", "Fancy"];
    reverseString(string)
    console.log(string)
@@ -20,7 +18,7 @@ function reverseString(str){
    ];
    function sumAges(people){
        let sum = 0
-       for (let i = o; i<people.length;i++){
+       for (let i = 0; i<people.length;i++){
            if(people[i].age <18){
                sum += people[i].age;
            }
@@ -32,7 +30,37 @@ function reverseString(str){
    
    // Using JS functions and an array of numbers, return positive if an element
    //  within the array is positive, negative if an element is negative, else zero
+   function findPositiveOrNegative(nums){
+    for (const num of nums) {
+        if (num >0) {
+            return "positive"
+        }
+        else if (num < 0) {
+            return "negative"
+        }
+        else{
+            return "zero"
+        }
+    }
+}
+let nums = [1,7,19,23,56]
+console.log(findPositiveOrNegative(nums));
    
-   // Given an array of objects, where each object represents an employee with an id, name, and salary property, 
-   // write a function that returns a new array of employee objects sorted by their salary in ascending order.
-git    
+ // Given an array of objects, where each object represents an employee with an id, name, and salary property, 
+ // write a function that returns a new array of employee objects sorted by their salary in ascending order.
+ function sortAscending(employee){
+    return sortEmployee = employee.sort((empl1, empl2) => empl1.salary - empl2.salary)
+ }
+ let employee = [
+    {id:12816970, name:'Terry', salary:20000},
+    {id:1281600, name:'Soyian', salary:80000},
+    {id:128880, name:'Fancy', salary:90000},
+
+ ];
+ console.log(sortAscending(employee))
+//  let people = [
+//     {name: 'Ann', age:20, height: '3ft'},
+//     {name: 'Henry', age:40, height: '4ft'},
+//     {name: 'Susan', age:30, height: '6ft'},
+
+// ];
